83e9fd0fa407b8130e15fd5c4566d0cebfb79700
solutechassistencia <solutechomeassistencia@gmail.com>
2025-11-20 17:02:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const galleries = {
     coifas: ["images/coifa.jpg","images/coifa2.jpg","images/coifa3.jpg","images/coifa4.jpg","images/coifa5.jpg"],
     fogoes: ["images/fogao.jpg","images/fogao2.jpg","images/fogao3.jpg","images/fogao4.jpg","images/fogao5.jpg", "images/fogao6.jpg", "images/fogao7.jpg", "images/fogao8.jpg", "images/fogao9.jpg", "images/fogao10.jpg"],
     lavadora: ["images/lavadora.jpg", "images/lavadora2.jpg", "images/lavadora3.jpg", "images/lavadora4.jpg", "images/lavadora5.jpg", "images/lavadora6.jpg", "images/lavadora7.jpg", "images/lavadora8.jpg", "images/lavadora9.jpg"],
-    geladeiras: ["images/geladeira.jpg","images/geladeira2.jpg","images/geladeira3.jpg","images/geladeira4.jpg","images/geladeira5.jpg"],
+    moveis: ["images/moveis.jpg","images/moveis2.jpg","images/moveis3.jpg","images/moveis4.jpg","images/moveis5.jpg"],
     purificadores: ["images/purificador.jpg","images/purificador2.jpg","images/purificador3.jpg","images/purificador4.jpg","images/purificador5.jpg"]
 };
 
